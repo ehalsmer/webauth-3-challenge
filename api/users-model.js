e10e8e653e353058ac7dbe238377e7c77b6d3e72
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-18 15:34:49 -0600

    Login is now returning a JWT token

--- api/users-model.js
+++ api/users-model.js
@@ -1,9 +1,25 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-    getUsers
+    getUsers,
+    findBy,
+    add,
+    findById
 }
 
 function getUsers(){
     return db('users')
+}
+
+function findBy(filter){
+    return db('users').where(filter);
+}
+
+async function add(user){
+    const [id] = await db('users').insert(user);
+    return findById(id);
+}
+
+function findById(id){
+    return db('users').where({ id }).first();
 }
--- api/users-router.js
+++ api/users-router.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Users = require('./users-model');
 
 const router = express.Router();
 
-router.get('/users', (req, res)=>{
+router.get('/users', (req, res) => {
     Users.getUsers()
     .then(response => {
         res.status(200).json(response)
@@ -14,5 +17,46 @@ router.get('/users', (req, res)=>{
     })
 })
 
+// todo: create middleware to check for required fields:
+// username (unique), password, department
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    Users.add(user)
+    .then(response => {
+        res.status(201).json(response);
+    })
+    .catch(error => {
+        res.status(500).json({message: 'error registering user'})
+    })
+})
+
+router.post('/login', (req, res) => {
+    let { username, password} = req.body;
+    Users.findBy({ username }).first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)){
+            const token = generateToken(user);
+            res.status(200).json({token});
+        } else {
+            res.status(401).json({message: 'Invalid credentials'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+function generateToken(user){
+    const payload = {
+        username: user.username
+    }
+    const options = {
+        expiresIn: '8h'
+    }
+    return jwt.sign(payload, 'server-secret', options)
+}
 
 module.exports = router;
